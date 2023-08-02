a51ace99085b3824c1496e4a9ae6667a6cf49085
Michael El Omar <michael.elomar@parrot.com>
2023-08-02 17:28:08 +0200

    read pioneer pose in ROS controller

--- src/vel_publisher.cpp
+++ src/vel_publisher.cpp
@@ -1,10 +1,21 @@
 #include <ros/ros.h>
 #include <std_msgs/Float32MultiArray.h>
+#include <geometry_msgs/Pose2D.h>
 
 #include <sstream>
 #include <iostream>
 #include <typeinfo>
 
+
+geometry_msgs::Pose2D pioneer_pose;
+
+void pose_callback(geometry_msgs::Pose2DPtr &msg)
+{
+    pioneer_pose.x = msg->x;
+    pioneer_pose.y = msg->y;
+    pioneer_pose.theta = msg->theta;
+    ROS_INFO("Read World Pose\n");
+}
 
 int main(int argc, char **argv)
 {
@@ -25,6 +36,8 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "vel_publisher");
     ros::NodeHandle n;
     ros::Publisher vel_pub = n.advertise<std_msgs::Float32MultiArray>("/pioneer_robot/vel_cmd", 1);
+    ros::Subscriber sub = n.subscribe("/pioneer_robot/pose", 1, pose_callback);
+
     ros::Rate loop_rate(10);
     while (ros::ok())
     {
@@ -34,8 +47,11 @@ int main(int argc, char **argv)
 
         vel_pub.publish(msg);
 
-        ros::spinOnce();
+        std::cout << "X: "<< pioneer_pose.x << std::endl;
+        std::cout << "Y: "<< pioneer_pose.y << std::endl;
+        std::cout << "Theta: "<< pioneer_pose.theta << std::endl;
 
+        ros::spinOnce();
         loop_rate.sleep();
     }
     return 0;
